4d0e84cbad3ef74dfdf71144dfbde3637e54142f
aysha8665 <aysha.amin@yandex.com>
2022-07-21 19:37:58 +0300

    enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,6 +5,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +41,20 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(color) => {
+                self.change_color(color);
+            }
+            Message::Echo(string) => {
+                self.echo(string)
+            }
+            Message::Move(point) => {
+                self.move_position(point);
+            }
+            Message::Quit => {
+                self.quit();
+            }
+        }
     }
 }
 
@@ -51,7 +69,7 @@ mod tests {
             position: Point{ x: 0, y: 0 },
             color: (0, 0, 0)
         };
-        state.process(Message::ChangeColor(255, 0, 255));
+        state.process(Message::ChangeColor((255, 0, 255)));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point{ x: 10, y: 15 }));
         state.process(Message::Quit);
