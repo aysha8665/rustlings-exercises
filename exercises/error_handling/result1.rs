d3aa28a2c41518ed5c4e1faca61bca286bcc4e8e
aysha8665 <aysha.amin@yandex.com>
2022-07-22 10:25:26 +0300

    error_handling done

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -14,7 +14,12 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            1.. => Ok(PositiveNonzeroInteger(value as u64)),
+            0 => Err(CreationError::Zero),
+            _ => Err(CreationError::Negative),
+        }
+
     }
 }
 
